884479aa30042a1217a9fed318f6cef86c5c5eb2
Ivan <84211523+ivan-the-noob@users.noreply.github.com>
2026-01-21 14:09:43 +0800

    a

--- features/admin/web/information.php
+++ features/admin/web/information.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-if (!isset($_SESSION['email']) || !isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    header("Location: ../../../users/web/api/login.php");
-    exit();
-}
+// if (!isset($_SESSION['email']) || !isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+//     header("Location: ../../../users/web/api/login.php");
+//     exit();
+// }
 
 include '../../../db.php';
 
